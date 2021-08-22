4a517ad6f86c5380eadaeea1b63e85402c7fb883
Kaasiak <kasia.kobalczyk@gmail.com>
2021-08-22 18:53:51 +0100

    Add weighted moving average

--- R/statistics.R
+++ R/statistics.R
@@ -146,6 +146,77 @@ SMA <- R6::R6Class("SMA", public = list(
 )
 )
 
+#' Create a streamer for calculating the weighted moving average
+#'
+#' @description \code{WMA} creates a streaming algorithm that can be used to
+#' keep track of the weighted mean of incoming values.
+#'
+#' In an n-day WMA the latest day has weight n, the second latest n-1, etc.,
+#' down to one.
+#'
+#'
+#' @docType class
+#'
+#' @examples
+#' mean <- WMA$new(c(1, 2))
+#' mean$update(c(3, 4))
+#' mean$value()
+#' #> [1] 7.5
+#'
+#' @export
+#' @format An \code{\link{R6Class}} generator object
+WMA <- R6::R6Class("WMA", public = list(
+    #' @description Creates a new \code{WMA} streamer object.
+    #'
+    #' @param x values to be used during initialisation (optional)
+    #'
+    #' @examples
+    #' weighted_mean <- WMA$new()
+    #'
+    #' @return The new \code{WMA} (invisibly)
+    initialize = function(x = NULL) {
+        if (!is.null(x)) {
+            private$count <- private$count + length(x)
+            private$weighted_sum <- seq(1, private$count) %*% x
+        }
+        invisible(self)
+    },
+    #' @description Resets the \code{WMA} streamer object.
+    #'
+    #' @param x values to be added to the stream
+    #'
+    #' @examples
+    #' weighted_mean <- WMA$new()
+    #' weighted_mean$update(c(1, 2))
+    #'
+    #' @return The updated \code{WMA} (invisibly)
+    update = function(x) {
+        n <- private$count
+        k <- length(x)
+        private$weighted_sum <- private$weighted_sum + seq(n + 1, n + k) %*% x
+        private$count <- n + k
+        invisible(self)
+    },
+    #' @description Returns the current value of the weighted average.
+    #'
+    #' @examples
+    #' weighted_mean <- WMA$new(c(1, 2, 3, 4))
+    #' weighted_mean$value()
+    #' #> [1] 7.5
+    #'
+    #' @return The updated \code{WMA} (invisibly)
+    value = function() {
+        if (private$count == 0L) {
+            return(0)
+        }
+        as.numeric(private$weighted_sum / private$count)
+    }
+), private = list(
+    weighted_sum = 0,
+    count = 0L
+)
+)
+
 #' Create a streamer for calculating the exponential moving average
 #'
 #' @description \code{EMA} creates a streaming algorithm that can be used to
